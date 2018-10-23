396d75ecf5733850269673f8ca658c42bb32b7fd
Greg Kallai <gerg.kallai@gmail.com>
2018-10-23 08:22:55 +0200

    error in the index found index.feature renamed, all test are passing

--- features/index.feature.js
+++ features/index.feature.js
@@ -14,7 +14,7 @@ describe('User can calculate BMI',() =>{
         await browser.close();
     });
     
-    it('by inputing his weight and heigth', async () => {
+    it('by inputing his weight and height', async () => {
         await browser.fillIn("input[id='weight-in-kgs']", {with: '95' })
         await browser.fillIn("input[id='height-in-cm']" ,{ with: '185'})
 
